afa7f92a463c173f5fc7a1808e0182af20673773
barkc040 <cj.barker@disney.com>
2017-04-28 12:59:31 -0700

    Added basic format for cal hashmap

---
+++ src/cal-dates2016-2017.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Seattle Public Schools (SPS) calendar dates
+ */
+var CAL_2016_2017 = {};     // Hashmap: Key is ISO8601 dates string format with value of object event
+
+CAL_2016_2017['2017-03-12T13:37:27+00:00'] = undefined;
+
+// TODO flush out
---
+++ src/cal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// TODO
+// Create utility calendar functions
+// Is current date weekend or weekday?
+// Load up calendar JSON or JavaScript dates
+// Convert ISO dates or create them accordingly depending on data type to transform
+// Check if date even loaded into memory/available (date or date range)?
+// Misc?
