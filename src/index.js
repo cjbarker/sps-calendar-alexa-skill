79feed38f84aaa9745e8347c8a18f8ceb2331dde
barkc040 <cj.barker@disney.com>
2017-04-29 15:02:09 -0700

    Created various utility functions for dates

--- src/index.js
+++ src/index.js
@@ -19,9 +19,88 @@ var output = "":
 
 // TODO create handler functions
 
+// Checks if string is empty, null, undefined or blank
+function isEmpty(str) {
+  return (!str || 0 === str.length || /^\s*$/.test(str));
+}
+
+// Converts ISO to CCYYMMDD, which is calendar event key in hashmap
+function iso2key(isoDate) {
+  var key = '';
+
+  if (isEmpty(isoDate)) {
+    return key; 
+  }
+
+  var date = new Date(isoDate);
+  if (isNaN(date) {
+    return key;
+  }
+
+  key = date.toISOString().substring(0, 10).replace(/-/g, '');
+  return key;
+}
+
+// Gets the current date and returns object of {iso: string date, key: iso hashmap key}
+function getToday() {
+  var isoDate = new Date().toISOString();
+
+  var today = {
+    iso: isoDate,
+    key: iso2key(isoDate)
+  };
+
+  return today;
+}
+
+// Converts an ISO date to date object for 
+function iso2obj(isoDate) {
+  if (isEmpty(isoDate) {
+    return undefined;
+  }
+
+  var d = new Date(isoDate);
+
+  var obj = {
+    day: fd,
+    date: 23,
+    month: kj,
+    year: kj,
+  };
+
+  return obj
+}
+
+function getOrdinal(num) {
+  if (isNaN(num)) {
+    return '';
+  }
+  
+  num = Math.abs(num);
+
+  if (num > 3 && num < 21) {
+    return 'th';
+  }
+
+  switch (num % 10) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
+}
+
+function isoObj2String(isoObj) {
+  if (isoObj === undefined) {
+    return '';
+  }
+  return isoObj.day + " the " + isoObj.date + getOrdinal(isoObj.date) + " of " + isoObj.month + " " + isoObj.year;
+}
+
 exports.handler = function(event, context, callback) {
   alexa = Alexa.handler(event, context);
   alexa.appId = APP_ID;
   //alexa.registerHandlers(handlers); TODO create handlers
   alexa.execute();
 };
+
