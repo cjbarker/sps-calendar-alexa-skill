30f691d9fd52b94ac99dcf90a0e0521c9223dc11
CJ Barker <cjbarker@gmail.com>
2017-05-01 13:05:59 -0700

    Added ordinal test

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ var expect = require('chai').expect;
 var sps = rewire('../index.js');
 
 isEmpty = sps.__get__('isEmpty');
+getOrdinal = sps.__get__('getOrdinal');
 
 describe ("SPS ALexa SKill Unit Tests", function () {
   describe("String empty utility check", function() {
@@ -16,5 +17,21 @@ describe ("SPS ALexa SKill Unit Tests", function () {
       expect(isEmpty(blank)).to.equal(true);
       expect(isEmpty(undef)).to.equal(true);
       expect(isEmpty(full)).to.equal(false);
+  });
+
+  describe("Convert numbers to ordinals", function() {
+    var st = 21;
+    var nd = 2;
+    var rd = 333;
+    var th = 5114;
+    var nan = 'nan fail';
+    var zero = 0;
+
+    expect(getOrdinal(st)).to.equal('st');
+    expect(getOrdinal(nd)).to.equal('nd');
+    expect(getOrdinal(rd)).to.equal('rd');
+    expect(getOrdinal(th)).to.equal('th');
+    expect(getOrdinal(zero)).to.equal('th');
+    expect(getOrdinal(nan)).to.equal('');
   });
 });
